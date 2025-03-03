ed6491cf4b6839009a5cd2f0f775e441753d8208
Andrew Beneker <andrewbeneker@gmail.com>
2025-03-03 15:40:47 -0500

    scaffolding complete

--- HelpDeskAPI/Entities/HelpDeskDbContext.cs
+++ HelpDeskAPI/Entities/HelpDeskDbContext.cs
@@ -15,12 +15,25 @@ public partial class HelpDeskDbContext : DbContext
     {
     }
 
+    public virtual DbSet<Customer> Customers { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-SBH023KO;Database=HelpDeskDB;Trusted_Connection=True;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer("Name=DefaultConnection");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Customer__3214EC075F397C00");
+
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.Email).HasMaxLength(60);
+            entity.Property(e => e.FirstName).HasMaxLength(30);
+            entity.Property(e => e.LastName).HasMaxLength(30);
+            entity.Property(e => e.Phone).HasMaxLength(15);
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 
